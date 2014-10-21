9463a934a4873c6f2f9ed62a0daf68949a59eed9
David Branner <brannerchinese@gmail.com>
2014-10-21 13:13:34 -0400

    simplify exceptions in step_to_midi; add tests

--- melodyoflyric/code/utils.py
+++ melodyoflyric/code/utils.py
@@ -7,16 +7,19 @@ step_to_pitch = {'C': 0, 'D': 2, 'E': 4, 'F': 5, 'G': 7, 'A': 9, 'B': 11}
 
 def step_to_midi(step, alter, octave):
     """Produce the MIDI note number and do basic validation."""
+    # Note that MIDI treats Middle C as note 60 (octave 5); MusicXML treats it
+    # as being the first note in octave 4. Hence we add 12, for the extra
+    # octave.
     if any([not isinstance(i, int) for i in [alter, octave]]):
-        raise Exception(
-                '''Pitch attributes alter, octave must be integers: {} {}'''.
-                format(alter, octave))
+        raise Exception#(
+#                '''Pitch attributes alter, octave must be integers: {} {}'''.
+#                format(alter, octave))
     if step not in step_to_pitch:
-        raise Exception('Step {} must be in step_to_pitch {}'.
-                format(step, step_to_pitch))
+        raise Exception#('Step {} must be in step_to_pitch {}'.
+#                format(step, step_to_pitch))
     midi_pitch = (octave * 12) + (step_to_pitch[step] + alter) + 12
     if not (0 <= midi_pitch <= 127):
-        raise Exception('midi_pitch {}; should be between 0 and 127'.
-            format(midi_pitch))
+        raise Exception#('midi_pitch {}; should be between 0 and 127'.
+#            format(midi_pitch))
     else:
         return midi_pitch
--- melodyoflyric/test/test_utils.py
+++ melodyoflyric/test/test_utils.py
@@ -9,7 +9,45 @@ import utils as U
 import pytest
 
 def test_step_to_midi_01():
+    """Test handling of Middle C."""
     step = 'C'
-    alter = 1
+    alter = 0
     octave = 4
-    assert U.step_to_midi('C', 0, 4) == 60
+    assert U.step_to_midi(step, alter, octave) == 60
+
+def test_step_to_midi_02():
+    """Test handling of non-integers."""
+    step = 'C'
+    alter = .1
+    octave = 4
+    with pytest.raises(Exception):
+        U.step_to_midi(step, alter, octave)
+    alter = 1
+    octave = .4
+    with pytest.raises(Exception):
+        U.step_to_midi(step, alter, octave)
+
+def test_step_to_midi_03():
+    """Test handling of negative results."""
+    step = 'C'
+    alter = -1
+    octave = -2
+    with pytest.raises(Exception):
+        U.step_to_midi(step, alter, octave)
+    alter = 1
+    octave = -4
+    with pytest.raises(Exception):
+        U.step_to_midi(step, alter, octave)
+
+def test_step_to_midi_04():
+    """Test handling of results above range."""
+    step = 'C'
+    alter = -1
+    octave = 10
+    with pytest.raises(Exception):
+        U.step_to_midi(step, alter, octave)
+    step = 'B'
+    alter = 1
+    octave = 9
+    with pytest.raises(Exception):
+        U.step_to_midi(step, alter, octave)
