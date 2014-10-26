3842a43f140bfc5b78b2a9a62809b9577c611625
David Branner <brannerchinese@gmail.com>
2014-10-25 21:11:52 -0400

    add new program process_poem.py

---
+++ melodyoflyric/code/process_poem.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# process_poem.py
+# David Prager Branner
+# 20141025
+
+import os
+import read_xml as R
+import utils as U
+
+def get_syllables(filename=os.path.join(
+        '..', 'data', 'sheu_ityng_pyiparshyng_20141009.xml')):
+    """Get all syllables of poem, their tones, and their durations."""
+    syllables = [(
+            syllable[0], 
+            U.identify_tone(syllable[0]), 
+            U.sum_syllable_durations(syllable)) 
+                for syllable in R.main(filename) if syllable[0]
+            ]
+    return syllables
+
